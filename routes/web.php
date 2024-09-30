b74c07d609efdb2b5d776e94fdedf1aa68561198
eltonanit <elton.ani@gmail.com>
2024-09-30 17:10:56 +0200

    [UPD] Added card-body for movie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\Guest\PageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/movies', [PageController::class, 'index'])->name('movies');
